b335e94c4a88207c9c243519c61189e678f79dee
TuvalSimha <tuval.simha@gmail.com>
2024-12-22 12:14:33 +0200

    setup gateway config

--- gateway.config.ts
+++ gateway.config.ts
@@ -4,4 +4,10 @@ export const gatewayConfig = defineConfig({
   landingPage: true,
   graphiql: true,
   cors: true,
+  supergraph: {
+    type: "hive",
+    endpoint:
+      "https://cdn.graphql-hive.com/artifacts/v1/f133ba73-44d2-435a-8d44-aed29bb0ae3c",
+    key: process.env.HIVE_CDN_KEY ?? "",
+  },
 });
